4b8152755011a15320b9861e69bc1089681d4930
Rio Yokota <rioyokota@gmail.com>
2018-03-02 19:52:43 +0900

    Revert to old ID that works

--- id.cpp
+++ id.cpp
@@ -2,23 +2,23 @@
 #include <sys/time.h>
 
 int main (int argc, char** argv) {
-  int ncols = 1000;
-  int nrows = 1000;
+  int n = 1000;
+  int m = 1000;
   int k = 16;
   int niter = 10;
-  double *M = (double*)malloc(nrows*ncols);
-  double *P = (double*)malloc(nrows*ncols);
-  for (int i=0; i<nrows; i++) {
-    for (int j=0; j<ncols; j++) {
+  gsl_matrix *M = gsl_matrix_alloc(m,n);
+  gsl_matrix *P = gsl_matrix_alloc(m,n);
+  for (int i=0; i<m; i++) {
+    for (int j=0; j<n; j++) {
       M->data[i*n+j] = 1 / fabs(i - j - n);
     }
   }
-  double *U,*S,*V;
+  gsl_matrix *U,*S,*V;
   struct timeval tic;
   gettimeofday(&tic, NULL);
   double error = 0;
   for(int it=0; it<niter; it++) {
-    randomized_low_rank_svd2(M, k, &U, &S, &V, nrows, ncols);
+    randomized_low_rank_svd2(M, k, &U, &S, &V);
     form_svd_product_matrix(U,S,V,P);
     error += get_percent_error_between_two_mats(M,P);
   }
@@ -26,10 +26,10 @@ int main (int argc, char** argv) {
   gettimeofday(&toc, NULL);
   double time = toc.tv_sec - tic.tv_sec + (toc.tv_usec - tic.tv_usec) * 1e-6;
   printf("time: %lf s, error: %g\n", time, error/niter);
-  free(M);
-  free(U);
-  free(S);
-  free(V);
-  free(P);
+  gsl_matrix_free(M);
+  gsl_matrix_free(U);
+  gsl_matrix_free(S);
+  gsl_matrix_free(V);
+  gsl_matrix_free(P);
   return 0;
 }
