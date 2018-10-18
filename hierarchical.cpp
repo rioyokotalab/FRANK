5443b5e29375ed3dd0f4b64c2df02ca132b9bc38
derpda <peter.spalthoff@gmail.com>
2018-10-18 16:29:27 +0900

    std::move used in Hierarchical as appropriate
    In the constructors from Dense and LowRank, temporary Dense and LowRank
    objects were created and then copied into *this(i, j). This is now done
    with move. This is more in line with C++11 move semantics and avoids
    unnecessary deep-copies of data.

--- hierarchical.cpp
+++ hierarchical.cpp
@@ -43,7 +43,7 @@ namespace hicma {
             D(ic,jc) = A(ic+i_begin,jc+j_begin);
           }
         }
-        (*this)(i,j) = D;
+        (*this)(i,j) = std::move(D);
       }
     }
   }
@@ -76,7 +76,7 @@ namespace hicma {
             LR.V(kc,jc) = A.V(kc,jc+j_begin);
           }
         }
-        (*this)(i,j) = LR;
+        (*this)(i,j) = std::move(LR);
       }
     }
   }
