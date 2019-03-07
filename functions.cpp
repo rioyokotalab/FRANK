b48fb8372c4795ac2670185d361995ded2c79829
Wawando <muhammad.ridwan.ab@gmail.com>
2019-03-07 16:44:37 +0900

    Added identity matrix generator

--- functions.cpp
+++ functions.cpp
@@ -22,6 +22,22 @@ namespace hicma {
     }
   }
 
+  void identity(
+                std::vector<double>& data,
+                std::vector<double>& x,
+                const int& ni,
+                const int& nj,
+                const int& i_begin,
+                const int& j_begin
+                ) {
+    for (int i=0; i<ni; i++) {
+      for (int j=0; j<nj; j++) {
+        if(i == j) data[i*nj+j] = 1;
+        else data[i*nj+j] = 0;
+      }
+    }
+  }
+
   void random(
               std::vector<double>& data,
               std::vector<double>& x,
