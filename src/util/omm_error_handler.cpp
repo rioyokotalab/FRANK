f836220b9cd45ff3687d3b0fa5af454cb6b40770
Peter Spalthoff <peter.spalthoff@gmail.com>
2020-03-19 20:00:57 +0900

    uint is not standard type. unsigned int used now

--- src/util/omm_error_handler.cpp
+++ src/util/omm_error_handler.cpp
@@ -18,7 +18,7 @@ void omm_error_handler(
   std::cerr << omm_name << "(";
   if (virtual_arguments.size() > 0) {
     std::cerr << virtual_arguments[0].get().type();
-    for (uint i=1; i<virtual_arguments.size(); ++i) {
+    for (unsigned int i=1; i<virtual_arguments.size(); ++i) {
       std::cerr << ", " << virtual_arguments[i].get().type();
     }
   }
