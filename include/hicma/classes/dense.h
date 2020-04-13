618fabf22641e8c7777a5765aef095d01f4f4cee
Peter Spalthoff <peter.spalthoff@gmail.com>
2020-04-13 17:26:40 +0900

    Unneeded constructor removed from Dense

--- include/hicma/classes/dense.h
+++ include/hicma/classes/dense.h
@@ -49,9 +49,6 @@ class Dense : public Node {
   // Explicit conversions using multiple-dispatch function.
   explicit Dense(const Node& A);
 
-  // Implicit conversion from NodeProxy (when returned from functions)
-  Dense(NodeProxy&& A);
-
   // Additional constructors
   Dense(int m, int n=1);
 
