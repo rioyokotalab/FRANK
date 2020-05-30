f5dd98e8f8b3c9ed8ebcd5d05e3658ff749e1101
Peter Spalthoff <peter.spalthoff@gmail.com>
2020-05-30 12:35:13 +0900

    LR addition test file now covers all versions

--- test/lr_add.cpp
+++ test/lr_add.cpp
@@ -11,16 +11,40 @@ int main() {
   int64_t N = 2048;
   int64_t rank = 128;
   std::vector<std::vector<double>> randx{get_sorted_random_vector(2*N)};
-  print("Time");
   timing::start("Init matrix");
   Dense D(laplacend, randx, N, N, 0, N);
   LowRank A(D, rank);
   LowRank B(D, rank);
-  timing::stopAndPrint("Init matrix");
-  timing::start("LR Add");
-  A += B;
-  timing::stopAndPrint("LR Add", 2);
-  print("Accuracy");
-  print("Rel. L2 Error", l2_error(D+D, A), false);
+  LowRank AWork(A);
+  timing::stop("Init matrix");
+
+  print("LR Add Default");
+  timing::start("LR Add Default");
+  AWork += B;
+  timing::stopAndPrint("LR Add Default", 2);
+  print("Rel. L2 Error", l2_error(D+D, AWork), false);
+
+  timing::start("Init matrix");
+  AWork = A;
+  timing::stop("Init matrix");
+  print("LR Add Naive");
+  updateCounter("LRA", 0);
+  timing::start("LR Add Naive");
+  AWork += B;
+  timing::stopAndPrint("LR Add Naive", 2);
+  print("Rel. L2 Error", l2_error(D+D, AWork), false);
+
+  timing::start("Init matrix");
+  AWork = A;
+  timing::stop("Init matrix");
+  print("LR Add Orthogonal");
+  updateCounter("LRA", 1);
+  timing::start("LR Add Orthogonal");
+  AWork += B;
+  timing::stopAndPrint("LR Add Orthogonal", 2);
+  print("Rel. L2 Error", l2_error(D+D, AWork), false);
+
+  print("-");
+  timing::printTime("Init matrix");
   return 0;
 }
