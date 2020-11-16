9e32b148b251c643883f0a11480db2805501ad5b
Wawando <muhammad.ridwan.ab@gmail.com>
2020-11-16 17:34:29 +0900

    Added initializer that reads matrix entries from text file

---
+++ include/hicma/classes/initialization_helpers/matrix_initializer_file.h
@@ -0,0 +1,54 @@
+#ifndef hicma_classes_initialization_helpers_matrix_initializer_file_h
+#define hicma_classes_initialization_helpers_matrix_initializer_file_h
+
+#include "hicma/classes/dense.h"
+#include "hicma/classes/initialization_helpers/matrix_initializer.h"
+
+#include <cstdint>
+
+
+namespace hicma
+{
+
+class ClusterTree;
+class IndexRange;
+
+class MatrixInitializerFile : public MatrixInitializer {
+ private:
+  std::string filename;
+  int ordering;
+ public:
+  // Special member functions
+  MatrixInitializerFile() = delete;
+
+  ~MatrixInitializerFile() = default;
+
+  MatrixInitializerFile(const MatrixInitializerFile& A) = delete;
+
+  MatrixInitializerFile& operator=(const MatrixInitializerFile& A) = delete;
+
+  MatrixInitializerFile(MatrixInitializerFile&& A) = delete;
+
+  MatrixInitializerFile& operator=(MatrixInitializerFile&& A) = default;
+
+  // Additional constructors
+  MatrixInitializerFile(
+    std::string filename, int ordering, int64_t admis, int64_t rank, int basis_type
+  );
+
+  // Utility methods
+  void fill_dense_representation(
+    Dense& A, const ClusterTree& node
+  ) const override;
+
+  void fill_dense_representation(
+    Dense& A, const IndexRange& row_range, const IndexRange& col_range
+  ) const override;
+
+  Dense get_dense_representation(const ClusterTree& node) const override;
+
+};
+
+} // namespace hicma
+
+#endif // hicma_classes_initialization_helpers_matrix_initializer_file_h
---
+++ src/classes/initialization_helpers/matrix_initializer_file.cpp
@@ -0,0 +1,71 @@
+#include "hicma/classes/initialization_helpers/matrix_initializer_file.h"
+
+#include "hicma/classes/dense.h"
+#include "hicma/classes/initialization_helpers/cluster_tree.h"
+#include "hicma/classes/initialization_helpers/index_range.h"
+
+#include <cstdint>
+#include <utility>
+#include <cassert>
+#include <fstream>
+
+
+namespace hicma
+{
+
+// Additional constructors
+MatrixInitializerFile::MatrixInitializerFile(
+  std::string filename, int ordering, int64_t admis, int64_t rank, int basis_type
+) : MatrixInitializer(admis, rank, basis_type), filename(filename), ordering(ordering) {}
+
+// Utility methods
+void MatrixInitializerFile::fill_dense_representation(
+  Dense& A, const ClusterTree& node
+) const {
+  fill_dense_representation(A, node.rows, node.cols);
+}
+
+void MatrixInitializerFile::fill_dense_representation(
+  Dense& A, const IndexRange& row_range, const IndexRange& col_range
+) const {
+  std::ifstream file;
+  file.open(filename);
+  int64_t dim[2];
+  double a;
+  file >>dim[0] >>dim[1];
+  assert(row_range.start+A.dim[0] <= dim[0]);
+  assert(col_range.start+A.dim[1] <= dim[1]);
+  if(ordering == MATRIX_ROW_MAJOR) {
+    for(int64_t i=0; i<std::min(dim[0], row_range.start+A.dim[0]); i++) {
+      for(int64_t j=0; j<dim[1]; j++) {
+        file >>a;
+        if((i >= row_range.start && j >= col_range.start)
+           && (i < row_range.start+A.dim[0] && j < col_range.start+A.dim[1])) {
+          A(i-row_range.start, j-col_range.start) = a;
+        }
+      }
+    }
+  }
+  else { //Assume entries in file are stored in column major
+    for(int64_t j=0; j<std::min(dim[1], col_range.start+A.dim[1]); j++) {
+      for(int64_t i=0; i<dim[0]; i++) {
+        file >>a;
+        if((i >= row_range.start && j >= col_range.start)
+           && (i < row_range.start+A.dim[0] && j < col_range.start+A.dim[1])) {
+          A(i-row_range.start, j-col_range.start) = a;
+        }
+      }
+    }
+  }
+  file.close();
+}
+
+Dense MatrixInitializerFile::get_dense_representation(
+  const ClusterTree& node
+) const {
+  Dense out(node.rows.n, node.cols.n);
+  fill_dense_representation(out, node.rows, node.cols);
+  return out;
+}
+
+} // namespace hicma
