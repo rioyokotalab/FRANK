80dfe1f8561303193ca79ef9ad6e42960fca7315
wawando <muhammad.ridwan.ab@gmail.com>
2021-12-07 12:44:58 +0900

    Rounded addition now works for rankA+rankB>block_size

--- examples/lr_add.cpp
+++ examples/lr_add.cpp
@@ -6,10 +6,10 @@
 
 using namespace hicma;
 
-int main() {
+int main(int argc, char** argv) {
   hicma::initialize();
-  int64_t N = 128;
-  int64_t rank = 16;
+  int64_t N = argc > 1 ? atoi(argv[1]) : 128;
+  int64_t rank = argc > 2 ? atoi(argv[2]) : 16;
   std::vector<std::vector<double>> randx{get_sorted_random_vector(2*N)};
   timing::start("Init matrix");
   Dense D(laplacend, randx, N, N, 0, N);
