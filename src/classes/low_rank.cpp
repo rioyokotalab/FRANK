b833d05e2ccec18779631105c741cf7fb2d70c6f
wawando <muhammad.ridwan.ab@gmail.com>
2022-03-07 10:40:05 +0900

    Added threshold based lowrank constructor

--- src/classes/low_rank.cpp
+++ src/classes/low_rank.cpp
@@ -5,9 +5,11 @@
 #include "hicma/classes/matrix.h"
 #include "hicma/classes/matrix_proxy.h"
 #include "hicma/extension_headers/classes.h"
+#include "hicma/operations/LAPACK.h"
 #include "hicma/operations/randomized_factorizations.h"
 #include "hicma/operations/misc.h"
 #include "hicma/util/omm_error_handler.h"
+#include "hicma/functions.h"
 
 #include "yorel/yomm2/cute.hpp"
 using yorel::yomm2::virtual_;
@@ -44,6 +46,15 @@ LowRank::LowRank(const Dense& A, int64_t rank)
   U = resize(U, dim[0], rank);
   V = resize(V, rank, dim[1]);
   S = resize(S, rank, rank);
+}
+
+LowRank::LowRank(const Dense& A, double eps)
+: Matrix(A), dim{A.dim[0], A.dim[1]}, eps(eps) {
+  std::tie(U, V) = truncated_geqp3(A, eps);
+  rank = U.dim[1];
+  // Set S as identity
+  std::vector<std::vector<double>> randx;
+  S = Dense(identity, randx, rank, rank);
 }
 
 LowRank::LowRank(const Matrix& U, const Dense& S, const Matrix& V, bool copy)
