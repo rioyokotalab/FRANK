f8dcc7e807fec566a4abe442dea4415b3cf5f39f
wawando <muhammad.ridwan.ab@gmail.com>
2022-04-06 16:02:18 +0900

    Refactor long if statements to switch case

--- src/operations/BLAS/trmm.cpp
+++ src/operations/BLAS/trmm.cpp
@@ -92,57 +92,63 @@ define_method(
   assert(A.dim[0] == (side == Side::Left ? B.dim[0] : B.dim[1]));
   assert(trans != 't'); //TODO implement for transposed case: need transposed gemm complete
   Hierarchical B_copy(B);
-  if(uplo == Mode::Upper) {
-    if(side == Side::Left) {
-      for(int64_t i=0; i<B.dim[0]; i++) {
-        for(int64_t j=0; j<B.dim[1]; j++) {
-          for(int64_t k=i; k<A.dim[1]; k++) {
-            if(k == i)
-              trmm(A(i, k), B(k, j), side, uplo, trans, diag, alpha);
-            else
-              gemm(A(i, k), B_copy(k, j), B(i, j), alpha, 1.);
+  switch(uplo) {
+    case Mode::Upper:
+      switch(side) {
+        case Side::Left:
+          for(int64_t i=0; i<B.dim[0]; i++) {
+            for(int64_t j=0; j<B.dim[1]; j++) {
+              for(int64_t k=i; k<A.dim[1]; k++) {
+                if(k == i)
+                  trmm(A(i, k), B(k, j), side, uplo, trans, diag, alpha);
+                else
+                  gemm(A(i, k), B_copy(k, j), B(i, j), alpha, 1.);
+              }
+            }
           }
-        }
-      }
-    }
-    else if(side == Side::Right) {
-      for(int64_t i=0; i<B.dim[0]; i++) {
-        for(int64_t j=0; j<B.dim[1]; j++) {
-          for(int64_t k=j; k>=0; k--) {
-            if(k == j)
-              trmm(A(k, j), B(i, k), side, uplo, trans, diag, alpha);
-            else
-              gemm(B_copy(i, k), A(k, j), B(i, j), alpha, 1);
+          break;
+        case Side::Right:
+          for(int64_t i=0; i<B.dim[0]; i++) {
+            for(int64_t j=0; j<B.dim[1]; j++) {
+              for(int64_t k=j; k>=0; k--) {
+                if(k == j)
+                  trmm(A(k, j), B(i, k), side, uplo, trans, diag, alpha);
+                else
+                  gemm(B_copy(i, k), A(k, j), B(i, j), alpha, 1);
+              }
+            }
           }
-        }
+          break;
       }
-    }
-  }
-  else if(uplo == Mode::Lower) {
-    if(side == Side::Left) {
-      for(int64_t i=0; i<B.dim[0]; i++) {
-        for(int64_t j=0; j<B.dim[1]; j++) {
-          for(int64_t k=i; k>=0; k--) {
-            if(k == i)
-              trmm(A(i, k), B(k, j), side, uplo, trans, diag, alpha);
-            else
-              gemm(A(i, k), B_copy(k, j), B(i, j), alpha, 1);
+      break;
+    case Mode::Lower:
+      switch(side) {
+        case Side::Left:
+          for(int64_t i=0; i<B.dim[0]; i++) {
+            for(int64_t j=0; j<B.dim[1]; j++) {
+              for(int64_t k=i; k>=0; k--) {
+                if(k == i)
+                  trmm(A(i, k), B(k, j), side, uplo, trans, diag, alpha);
+                else
+                  gemm(A(i, k), B_copy(k, j), B(i, j), alpha, 1);
+              }
+            }
           }
-        }
-      }
-    }
-    else if(side == Side::Right) {
-      for(int64_t i=0; i<B.dim[0]; i++) {
-        for(int64_t j=0; j<B.dim[1]; j++) {
-          for(int64_t k=j; k<B.dim[1]; k++) {
-            if(k == j)
-              trmm(A(k, j), B(i, k), side, uplo, trans, diag, alpha);
-            else
-              gemm(B_copy(i, k), A(k, j), B(i, j), alpha, 1);
+          break;
+        case Side::Right:
+          for(int64_t i=0; i<B.dim[0]; i++) {
+            for(int64_t j=0; j<B.dim[1]; j++) {
+              for(int64_t k=j; k<B.dim[1]; k++) {
+                if(k == j)
+                  trmm(A(k, j), B(i, k), side, uplo, trans, diag, alpha);
+                else
+                  gemm(B_copy(i, k), A(k, j), B(i, j), alpha, 1);
+              }
+            }
           }
-        }
+          break;
       }
-    }
+      break;
   }
 }
 
